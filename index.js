331c73ebf59f2dfc95037ada6e990aca7f5a614a
ismahaneyy <liciousmayrah@gmail.com>
2022-12-15 10:46:33 +0300

    fetched data from the api, added even listener click, where, when the like and favourite button are clicked the color changes to red and when a new quote loads the original color is displayed also when the quotebtn is clicked a new quote loads with the authors name

--- index.js
+++ index.js
@@ -4,12 +4,35 @@ let quoteBtn = document.querySelector("button#quote")
 let favouriteBtn = document.querySelector("#Favourite")
 let likeBtn = document.querySelector(".like")
 
-const BaseUrl = "http://api.quotable.io/random"
+const BaseUrl = "http://api.quotable.io/random" // gets the API
+
 function randomQuote(){
-    fetch(BaseUrl) // geting the API
+  quoteBtn.innerText = "loading Quote..." // while a new quote is loading, the text in the quotebtn reads loading Quote...
+    fetch(BaseUrl) // gets the API
     .then(res => res.json()) //brings data as a promise
-    .then(data => {console.log(data);//returns the data
-    })}randomQuote()
+    .then(data => {console.log(data); //returns the data
+    quoteText.innerText = data.content;
+    authorName.innerText = data.author;
+    
+    likeBtn.addEventListener("click", ()=>{
+      likeBtn.style.color = "Red" //changing the like button to red when clicked
+    })
+
+    favouriteBtn.addEventListener("click", ()=>{
+    favouriteBtn.style.color ="Red"//changing the favourite button to red when clicked
+  },{once: true})
+  
+    quoteBtn.innerText = "New Quote"  //when the quote loads the button text changes to new quote
+    likeBtn.style.color = "#7b2cbf"  // the color of the likebutton changed back to the original color when a new quote is displayed 
+    favouriteBtn.style.color = "#7b2cbf" // the color of the favouritebutton changed back to the original color when a new quote is displayed 
+    })
+  let copyBtn = document.querySelector(".copy") // to copy the quote to clipboard
+  copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText)
+  })
+
+quoteBtn.addEventListener("click", randomQuote); // when clicked a new quote is dispalyed
+}randomQuote()
 
 
     function signupForm() {
